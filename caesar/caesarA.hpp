4bde6dfe5d0cac752c7729006b3e4a1ae79a393e
Angelinashev <angelu3a@gmail.com>
2025-04-22 21:57:13 +0300

    fix decryption

--- caesar/caesarA.hpp
+++ caesar/caesarA.hpp
@@ -10,11 +10,18 @@ using namespace std;
 string Caesar(const string& LINE, const string& alphabet, int key) {
 	string result = "";
 	int len = alphabet.length();
-	for (char b : LINE) {
+	for (int i = 0; i<LINE.length(); i++) {
+		char b = LINE[i];
 		int num = alphabet.find(b);
 		if (num != string::npos) {
-			int new_index = (num + key + len) % len;
-			result += alphabet[new_index];
+			int new_index = (num + key) % len;
+			if (new_index < 0) {
+				new_index += len;
+			}
+			printf("\n%c-%d-%d", b, num, new_index);
+			char e = alphabet[new_index];
+			result += e;
+			printf("-%c", e);
 		}
 		else {
 			result += b; // Yakshcho symbol ne z alfavitu — dodai yogo bez zmin
@@ -32,7 +39,7 @@ string Caesar2(const string& LINE, const string& alphabet, int key) {
 void Execute1() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int choice, Key;
+	int choice=1, Key;
 	string inputLINE;
 	string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
@@ -40,19 +47,16 @@ void Execute1() {
 	cin >> Key;
 	cin.ignore();
 	cout << "Enter word for encryption/decryption: ";
-	getline(cin, inputLINE);
-	for (char& c : inputLINE) {
-		c = toupper(c);
-	}
+	cin>>inputLINE;
 
 	if (choice == 1) {
 		string shifr = Caesar(inputLINE, alphabet, Key);
-		cout << shifr;
+		cout << "\n" << shifr;
 
 	}
 	else if (choice == 2) {
 		string deshifr = Caesar(inputLINE, alphabet, -Key);
-		cout << deshifr;
+		cout <<"\n" << deshifr;
 	}
 }
 
