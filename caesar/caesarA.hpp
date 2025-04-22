7420b3c7b01f610f19e4b6c49de15e6ae4019fcf
voodoomeggy <voodoomeggy@gmail.com>
2025-04-22 20:44:52 +0300

    fix tab

--- caesar/caesarA.hpp
+++ caesar/caesarA.hpp
@@ -8,50 +8,50 @@ using namespace std;
 
 // Функція шифрування Цезаря
 string Caesar(const string& LINE, const string& alphabet, int key) {
-  string result = ""; 
-  int len = alphabet.length();
-  for (char b : LINE) {
-    int num = alphabet.find(b);
-    if (num != string::npos) {
-      int new_index = (num + key + len) % len; 
-      result += alphabet[new_index];
-    }
-    else {
-      result += b; // Yakshcho symbol ne z alfavitu — dodai yogo bez zmin
-    }
-  }
-  return result;
+	string result = "";
+	int len = alphabet.length();
+	for (char b : LINE) {
+		int num = alphabet.find(b);
+		if (num != string::npos) {
+			int new_index = (num + key + len) % len;
+			result += alphabet[new_index];
+		}
+		else {
+			result += b; // Yakshcho symbol ne z alfavitu — dodai yogo bez zmin
+		}
+	}
+	return result;
 }
 
 // Функція дешифрування (використовує ту ж саму Caesar, але з від’ємним ключем)
 string Caesar2(const string& LINE, const string& alphabet, int key) {
-  return Caesar(LINE, alphabet, -key);
+	return Caesar(LINE, alphabet, -key);
 }
 
 // Головна функція виклику
 void Execute1() {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    int choice, Key;
-    string inputLINE;
-    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int choice, Key;
+	string inputLINE;
+	string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-  cout << "Enter key: ";
-  cin >> Key;
-  cin.ignore(); 
-  cout << "Enter word for encryption/decryption: ";
-  getline(cin, inputLINE);
-  for (char& c : inputLINE) {
-    c = toupper(c);
-  }
+	cout << "Enter key: ";
+	cin >> Key;
+	cin.ignore();
+	cout << "Enter word for encryption/decryption: ";
+	getline(cin, inputLINE);
+	for (char& c : inputLINE) {
+		c = toupper(c);
+	}
 
-    if (choice == 1) {
-        string shifr = Caesar(inputLINE, alphabet, Key);
-        cout << shifr;
- 
-    }
-    else if (choice == 2) {
-        string deshifr = Caesar(inputLINE, alphabet, -Key);
-        cout << deshifr;
-}
+	if (choice == 1) {
+		string shifr = Caesar(inputLINE, alphabet, Key);
+		cout << shifr;
+
+	}
+	else if (choice == 2) {
+		string deshifr = Caesar(inputLINE, alphabet, -Key);
+		cout << deshifr;
+	}
 
